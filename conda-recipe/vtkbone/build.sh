cf3bd88b86dc8e9ff02fdcc0752e9da46861349a
Bryce Besler <babesler@ucalgary.ca>
2019-11-20 20:56:15 -0700

    Add compiler to build of meta.yaml

--- conda-recipe/vtkbone/build.sh
+++ conda-recipe/vtkbone/build.sh
@@ -10,17 +10,24 @@ case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
 		export PYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_VER}.so"
 		export PYTHON_INCLUDE_DIR="${PREFIX}/include/python${PY_VER}"
+
+		# CMAKE_PLATFORM_FLAGS+=(-DCMAKE_FIND_ROOT_PATH="${PREFIX};${BUILD_PREFIX}/${HOST}/sysroot")
     ;;
   darwin*)
 		export PYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_VER}.dylib"
 		export PYTHON_INCLUDE_DIR="${PREFIX}/include/python${PY_VER}"
+
+		# Get the SDK
+		CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+		export DYLD_FALLBACK_LIBRARY_PATH="${BUILD_PREFIX}/lib/:${DYLD_LIBRARY_PATH}"
     ;;
   *)
 esac
 
-# C/CXX Flags
-export CFLAGS="-fPIC"
-export CXXFLAGS="-fPIC"
+echo $LD_LIBRARY_PATH
+echo $PATH
+conda env list
+
 
 # CMake
 cmake .. \
@@ -33,7 +40,8 @@ cmake .. \
 	-DBOOST_ROOT:PATH="${PREFIX}" \
 	-DPYTHON_LIBRARY:FILEPATH="${PYTHON_LIBRARY}" \
 	-DPYTHON_INCLUDE_DIR:PATH="${PYTHON_INCLUDE_DIR}" \
-	-DENABLE_TESTING:BOOL=ON
+	-DENABLE_TESTING:BOOL=ON \
+	"${CMAKE_PLATFORM_FLAGS[@]}"
 
 # Compile and install
 ninja install -v
