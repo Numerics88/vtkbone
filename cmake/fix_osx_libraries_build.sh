8a5560a3e0e03629f6433e50e5cb746dffabaa0c
friedchickean <kdarg29@gmail.com>
2025-05-12 15:25:53 -0600

    Figure out why libpthread is forced to Macosx11.1

--- cmake/fix_osx_libraries_build.sh
+++ cmake/fix_osx_libraries_build.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
-#
-# This script must be run from the directory containing the library or executable to modify.
+# #!/bin/bash
+# #
+# # This script must be run from the directory containing the library or executable to modify.
 
-libpath=libvtkbone.dylib
-insertString=/Users/Shared/install/vtk-6.3.0/lib/
+# libpath=libvtkbone.dylib
+# insertString=/Users/Shared/install/vtk-6.3.0/lib/
 
-# Iterate over dependencies
-for myfile in $(otool -L $libpath | grep libvtk | grep -v vtkbone | awk '{print $1}')
-do
-   # Skip system libraries
-   if [[ "$myfile" == /usr/lib/* || "$myfile" == /Library/Developer/CommandLineTools/* ]]; then
-      echo "Skipping system library: $myfile"
-      continue
-   fi
+# # Iterate over dependencies
+# for myfile in $(otool -L $libpath | grep libvtk | grep -v vtkbone | awk '{print $1}')
+# do
+#    # Skip system libraries
+#    if [[ "$myfile" == /usr/lib/* || "$myfile" == /Library/Developer/CommandLineTools/* ]]; then
+#       echo "Skipping system library: $myfile"
+#       continue
+#    fi
 
-   # Modify library path
-   echo "Changing $myfile to $insertString$(basename $myfile)"
-   install_name_tool -change "$myfile" "$insertString$(basename $myfile)" "$libpath"
-done
+#    # Modify library path
+#    echo "Changing $myfile to $insertString$(basename $myfile)"
+#    install_name_tool -change "$myfile" "$insertString$(basename $myfile)" "$libpath"
+# done
